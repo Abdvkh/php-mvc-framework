52d72c29fcccc90fd0a543e166d399031cf26ac6
abdvkh <e-address@bk.ru>
2020-11-27 13:20:19 +0500

    Added controlers; Some routing changes; Layout changing; etc...

--- public/index.php
+++ public/index.php
@@ -1,16 +1,20 @@
 <?php
 
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use app\core\Application;
+use app\controllers\SiteController;
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
-$app->router->get('/', function(){
-   return "Hello world!";
-});
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/contact', 'contact');
 
-$app->router->get('/contact', function(){
-    return "Contact";
-});
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [\app\controllers\AuthController::class, 'login']);
+$app->router->post('/login', [\app\controllers\AuthController::class, 'login']);
+$app->router->get('/register', [\app\controllers\AuthController::class, 'register']);
+$app->router->post('/register', [\app\controllers\AuthController::class, 'register']);
 
 $app->run();
