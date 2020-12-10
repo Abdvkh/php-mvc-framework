56ce92239ad9644a294b41e421199ecd9452fffc
abdvkh <e-address@bk.ru>
2020-12-10 15:34:14 +0500

    User friendly labels;

--- core/Model.php
+++ core/Model.php
@@ -25,6 +25,16 @@ abstract class Model
 
     abstract public function rules() : array;
 
+    public function labels()
+    {
+        return [];
+    }
+
+    public function getLabel($attribute)
+    {
+        return $this->labels()[$attribute] ?? $attribute;
+    }
+
     public function validate()
     {
         foreach ($this->rules() as $attribute => $rules){
@@ -53,6 +63,7 @@ abstract class Model
                 }
 
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}){
+                    $rule['match'] = $this->getLabel($rule['match']);
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
 
@@ -65,7 +76,7 @@ abstract class Model
                     $statement->execute();
                     $record = $statement->fetchObject();
                     if ($record) {
-                        $this->addError($attribute, self::RULE_UNIQUE, ['field'=>$attribute]);
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $this->getLabel($attribute)]);
                     }
                 }
             }
